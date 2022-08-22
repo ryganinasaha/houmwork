4491622c855d44ed51e05a06d6ed78ea8b8fb3d6
Sasha <ryganinnan@gmail.com>
2022-08-22 21:12:57 +0300

    22_08_2022

--- 17_08_2022/src/car.js
+++ 17_08_2022/src/car.js
@@ -1,5 +1,53 @@
-const car = {
-    wheel: 4
+let cars = []
+
+function getCars() {
+    return cars
 }
 
-module.exports = car;
+function getCar(id) {
+    return cars.find(item => item.id === id)
+}
+
+function addCar(newCar) {
+    newCar.id = generateUniqId()
+    cars.push(newCar)
+    return newCar.id
+}
+function deleteCar(id) {
+
+}
+
+function updateCar(id, car) {
+
+}
+ 
+function reset() {
+    cars.length = 0
+}
+
+function init(initCars) {
+    cars = initCars
+}
+
+function generateUniqId() {
+    let max = 0
+    for (let i = 0; i< cars.length; i++){
+        if (cars[i].id > max){
+            max = cars[i].id
+        }
+    }
+    return max + 1
+}
+
+module.exports = {
+    getCars,
+    getCar,
+    addCar,
+    deleteCar,
+    updateCar,
+    reset,
+    init
+};
+    
+
+    
--- 17_08_2022/src/car.test.js
+++ 17_08_2022/src/car.test.js
@@ -1,5 +1,86 @@
 const car = require('./car');
 
-test('car mast have 4 wheel', () => {
-    expect(car.wheel).toBe(4)
-});
+
+beforeEach(() => {
+
+    car.init([
+        {
+            id: 1,
+            name: 'Lada 2106',
+            color: 'red',
+            year: 2000,
+            price: 500,
+            currency: 'RUB'
+        },
+        {
+            id: 2,
+            name: 'Lada Kalina',
+            color: 'black',
+            year: 2019,
+            price: 50000,
+            currency: 'RUB'
+        }
+    ])
+})
+
+describe('getCars', () =>{
+    test('должен вернуть массив машин', () => {
+        expect(car.getCars()).toEqual([
+            {
+                id: 1,
+                name: 'Lada 2106',
+                color: 'red',
+                year: 2000,
+                price: 500,
+                currency: 'RUB'
+            },
+            {
+                id: 2,
+                name: 'Lada Kalina',
+                color: 'black',
+                year: 2019,
+                price: 50000,
+                currency: 'RUB'
+            }
+        ])
+    })
+})
+
+describe('getCar', () => {
+    test('получение одного объекта по id', () => {
+        expect(car.getCar(2)).toEqual(
+            {
+                id: 2,
+                name: 'Lada Kalina',
+                color: 'black',
+                year: 2019,
+                price: 50000,
+                currency: 'RUB'
+            }
+        ) 
+    })
+    test('должен вернуть undefined не существующем id', () => {
+        expect(car.getCar(500)).toEqual(undefined)
+    })
+})
+
+describe('addCar', () => {
+    test('добовляет новый элемент в массив с уникальным id', () => {
+        const newId = car.addCar({
+            name: 'Reno Daster',
+            color: 'pinл',
+            year: 2020,
+            price: 60000,
+            currency: 'RUB'})
+        expect(newId).toEqual(3)
+        expect(car.getCar(newId)).toEqual({
+            id: 3,
+            name: 'Reno Daster',
+            color: 'pinл',
+            year: 2020,
+            price: 60000,
+            currency: 'RUB'
+        })
+    })
+})
+
